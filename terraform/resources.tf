4c0788844e33bb5a1512b8a0a54cc2f25bdabdda
Eyal Cohen Haver <mellvinch@gmail.com>
2025-01-07 03:05:50 +0200

    a

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -12,19 +12,19 @@ associate_public_ip_address = true
   }
 }
 
-# #CICD EC2
-# resource "aws_instance" "cicd_ec2" {
-#   ami                    = "ami-0fff1b9a61dec8a5f"
-#   instance_type          = "t2.micro"
-#   key_name               = "vockey"
-#   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-#   subnet_id = aws_subnet.public_subnet.id
-#   #user_data = file("./cicd_user_data.sh")
-# associate_public_ip_address = true
-#   tags = {
-#     Name = "cicd_ec2"
-#   }
-# }
+#CICD EC2
+resource "aws_instance" "cicd_ec2" {
+  ami                    = "ami-0fff1b9a61dec8a5f"
+  instance_type          = "t2.micro"
+  key_name               = "vockey"
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  subnet_id = aws_subnet.public_subnet.id
+  #user_data = file("./cicd_user_data.sh")
+associate_public_ip_address = true
+  tags = {
+    Name = "cicd_ec2"
+  }
+}
 
 #Security Group
 resource "aws_security_group" "ec2_sg" {
