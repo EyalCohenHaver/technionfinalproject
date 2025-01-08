7cbdaaf870ca4683242389068edefeed24418ae5
Eyal Cohen Haver <mellvinch@gmail.com>
2025-01-08 19:48:52 +0200

    a

--- terraform/ec2_user_data.sh
+++ terraform/ec2_user_data.sh
@@ -11,3 +11,9 @@ sudo yum remove awscli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+aws configure <<EOF
+your-access-key-id
+your-secret-access-key
+None
+None
+EOF
