2f29a4c20958a359d55ec89aec998a54e8acaa5c
Eyal Cohen Haver <mellvinch@gmail.com>
2025-01-08 15:23:09 +0200

    a

--- terraform/ec2_user_data.sh
+++ terraform/ec2_user_data.sh
@@ -4,6 +4,10 @@ sudo yum update -y
 sudo yum install -y docker
 sudo systemctl enable docker
 sudo systemctl start docker
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
 
 mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.321.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.321.0/actions-runner-linux-x64-2.321.0.tar.gz
@@ -11,5 +15,5 @@ sudo yum install -y perl-Digest-SHA
 echo "ba46ba7ce3a4d7236b16fbe44419fb453bc08f866b24f04d549ec89f1722a29e  actions-runner-linux-x64-2.321.0.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-2.321.0.tar.gz
 sudo yum install -y libicu
-./config.sh --url https://github.com/EyalCohenHaver/technionfinalproject --token BIH6JNTOWYGIR3Z2WRB44UDHPUQRS
-./run.sh
+sudo -u ec2-user ./config.sh --url https://github.com/EyalCohenHaver/technionfinalproject --token BIH6JNTOWYGIR3Z2WRB44UDHPUQRS
+sudo -u ec2-user ./run.sh
